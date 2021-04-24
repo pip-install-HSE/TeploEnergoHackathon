5be96f644d2a17ce75ccfba090b7d0cedf10d4eb
6a16ec <semaev2014@yandex.ru>
2021-04-25 00:09:04 +0300

    New version by 6a16ec

--- database/create_tables.py
+++ database/create_tables.py
@@ -2,16 +2,15 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 from peewee import *
+import pandas as pd
 import datetime
 
-
-db = SqliteDatabase('my_database.db')
 db = PostgresqlDatabase(
     database=os.environ.get("DB_NAME"),
     user=os.environ.get("DB_USERNAME"),
     password=os.environ.get("DB_PASSWORD"),
     host=os.environ.get("DB_HOST"),
-    port=os.environ.get("DB_HOST")
+    port=os.environ.get("DB_PORT")
 )
 
 
@@ -19,11 +18,27 @@ class BaseModel(Model):
     class Meta:
         database = db
 
-class User(BaseModel):
-    username = CharField(unique=True)
+class Customer(BaseModel):
+    name = CharField(unique=True)
 
-class Tweet(BaseModel):
-    user = ForeignKeyField(User, backref='tweets')
-    message = TextField()
-    created_date = DateTimeField(default=datetime.datetime.now)
-    is_published = BooleanField(default=True)
+class Service(BaseModel):
+    name = CharField(unique=True)
+# class Order(BaseModel):
+#     numb
+
+# class Tweet(BaseModel):
+#     user = ForeignKeyField(User, backref='tweets')
+#     message = TextField()
+#     created_date = DateTimeField(default=datetime.datetime.now)
+#     is_published = BooleanField(default=True)
+db.drop_tables([Customer, Service])
+db.connect(reuse_if_open=True)
+db.create_tables([Customer, Service])
+
+data_frame = pd.read_excel(io='./orders.xlsx', sheet_name='Документы')
+for el in data_frame['Заказчик']:
+
+    if el not in Customer.select(Customer.name):
+        Customer.insert(name=el).execute()
+
+
