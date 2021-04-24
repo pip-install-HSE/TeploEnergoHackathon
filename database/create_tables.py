a8e8d28f8471f93bb0ffc54a42feba428230e574
6a16ec <semaev2014@yandex.ru>
2021-04-25 01:43:42 +0300

    Orders excel file parsed

--- database/create_tables.py
+++ database/create_tables.py
@@ -19,12 +19,22 @@ class BaseModel(Model):
         database = db
 
 class Customer(BaseModel):
-    name = CharField(unique=True)
+    name = CharField(null=False, unique=True)
+
+class Vehicle(BaseModel):
+    name = CharField(null=False, unique=True)
+
+class License(BaseModel):
+    name = CharField(null=False, unique=True, max_length=16)
 
 # class Service(BaseModel):
 #     name = CharField(unique=True)
 
-# class Order(BaseModel):
+class Order(BaseModel):
+    customer = ForeignKeyField(Customer, null=False, backref='orders')
+    vehicle = ForeignKeyField(Vehicle, null=False, backref='orders')
+    license = ForeignKeyField(License, null=False, backref='orders')
+    is_completed = BooleanField(null=False)
 
 # class Order(BaseModel):
 #     numb
@@ -34,18 +44,49 @@ class Customer(BaseModel):
 #     message = TextField()
 #     created_date = DateTimeField(default=datetime.datetime.now)
 #     is_published = BooleanField(default=True)
-try:
-    db.drop_tables([Customer])
-except:
-    print('Nothing to delete')
 db.connect(reuse_if_open=True)
-db.create_tables([Customer])
+tables = [Order, Customer, Vehicle, License]
+for table in tables:
+    try:
+        db.drop_tables([table])
+    except:
+        print(f'Can not delete {table}')
+db.create_tables(tables)
 
-data_frame = pd.read_excel(io='./orders.xlsx', sheet_name='Документы')
+orders_data_frame = pd.read_excel(io='./orders.xlsx', sheet_name='Документы')
 
-for el in data_frame['Заказчик']:
+for el in orders_data_frame['Заказчик']:
     if not Customer.select().where(Customer.name == el):
         Customer.insert(name=el).execute()
+
+for el in orders_data_frame['Требование_кТС']:
+    if not Vehicle.select().where(Vehicle.name == el):
+        Vehicle.insert(name=el).execute()
+
+for el in orders_data_frame['НазначеноТС']:
+    if not License.select().where(License.name == el):
+        License.insert(name=el).execute()
+
+for i, row in orders_data_frame.iterrows():
+    if i > 5:
+        break
+    Order.insert(
+        customer=Customer.select().where(Customer.name == row['Заказчик']),
+        vehicle=Vehicle.select().where(Vehicle.name == row['Требование_кТС']),
+        license=License.select().where(License.name == row['НазначеноТС']),
+        is_completed=True if 'исполнен' in row['СтатусЗаявки'].lower() else False
+    ).execute()
+
+
+    # print(row['Заказчик'], row['СтатусЗаявки'])
+
+# for line in data_frame[:5]:
+#     print(line)
+
+# for el in data_frame['СтатусЗаявки']:
+#     if not OrderStatus.select().where(OrderStatus.name == el):
+#         Customer.insert(name=el).execute()
+
 
 # for el in data_frame['ВидОперации']:
 #     if not Service.select().where(Service.name == el):
