2571bfd7321b999ce907b70562382ae07d7c0e0f
TolimanStaR <dadyryaev@edu.hse.ru>
2021-04-24 18:23:59 +0300

    views

---
+++ transport/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path(
+        '',
+        views.static,
+        'main-page',
+    ),
+    path(
+        'main/',
+        views.MainView.as_view(),
+        'main-page-template',
+    )
+]
--- transport/views.py
+++ transport/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+def static(req):
+    html = open("transport-web/dist/index.html").read()
+    return HttpResponse(html)
+
+
+class MainView(TemplateView):
+    template_name = 'transport-web/dist/index.html'
