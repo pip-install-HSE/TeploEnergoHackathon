44e42378f99edabc7ca977aa7990c3ec5eafe739
TolimanStaR <dadyryaev@edu.hse.ru>
2021-04-24 18:50:04 +0300

    update

--- transport/urls.py
+++ transport/urls.py
@@ -3,14 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path(
-        '',
-        views.static,
-        name='main-page',
-    ),
-    path(
-        'main/',
-        views.MainView.as_view(),
-        name='main-page-template',
-    )
+    path('',
+         views.static,
+         name='main-page', ),
+    path('main/',
+         views.MainView.as_view(),
+         name='main-page-template', ),
+    path('<str:something>/', views.static, name='all'),
 ]
--- transport/views.py
+++ transport/views.py
@@ -5,7 +5,6 @@ from django.views.generic import TemplateView
 
 def static(request):
     html = open("transport-web/dist/index.html").read()
-    print(html)
     return HttpResponse(html)
 
 
