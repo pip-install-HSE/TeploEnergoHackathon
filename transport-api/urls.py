3576f7495a9631ca5865d30b8a370acffb9c1d61
TolimanStaR <dadyryaev@edu.hse.ru>
2021-04-24 19:35:52 +0300

    update

--- transport-api/urls.py
+++ transport-api/urls.py
@@ -17,8 +17,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-
 urlpatterns = [
+                  path('/', include('transport.urls')),
                   path('admin/', admin.site.urls),
-                  path('/root/TeploEnergoHackathon/transport-web/dist/', include('transport.urls'))
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
