4b3568bde84a8c63c89840c2a5b8e7eb06326e22
TolimanStaR <dadyryaev@edu.hse.ru>
2021-04-25 00:24:22 +0300

    update

---
+++ script.py
@@ -0,0 +1,11 @@
+import csv
+import xlrd
+
+
+import pandas as pd
+
+import openpyxl
+
+
+data_xls = pd.read_excel('Putevye_listy.xlsx', dtype=str, engine='openpyxl', sheet_name='Документы')
+data_xls.to_csv('csvfile.csv', encoding='utf-8', index=False)
