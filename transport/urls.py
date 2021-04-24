3b27bee4ced87346a74d11da000041ba284c7aeb
TolimanStaR <dadyryaev@edu.hse.ru>
2021-04-24 19:40:49 +0300

    update

--- transport/urls.py
+++ transport/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='index'),
     path('/root/TeploEnergoHackathon/transport-web/dist/', views.file, name='file1'),
     path('<str:file_name>/', views.file, name='file'),
+    path('', views.index, name='index'),
 ]
