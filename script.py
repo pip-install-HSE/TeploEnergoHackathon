fcc7dc6c9d7d5dacc509daad11fe0acd57927630
TolimanStaR <dadyryaev@edu.hse.ru>
2021-04-25 00:28:59 +0300

    update

--- script.py
+++ script.py
@@ -9,3 +9,4 @@ import openpyxl
 
 data_xls = pd.read_excel('Putevye_listy.xlsx', dtype=str, engine='openpyxl', sheet_name='Документы')
 data_xls.to_csv('csvfile.csv', encoding='utf-8', index=False)
+1
