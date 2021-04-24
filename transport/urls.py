01ccde780dea38ac21becfe983e1e2598a892b3c
TolimanStaR <dadyryaev@edu.hse.ru>
2021-04-24 19:12:00 +0300

    update

--- transport/urls.py
+++ transport/urls.py
@@ -4,9 +4,5 @@ from . import views
 
 
 urlpatterns = [
-    path('',
-         views.static,
-         name='main-page', ),
     path('<str:file_name>/', views.file, name='file'),
-    path('<str:something>/', views.static, name='all'),
 ]
