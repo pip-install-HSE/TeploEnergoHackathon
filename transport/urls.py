ded559143bc50909414ba1a15659ff115aca7200
TolimanStaR <dadyryaev@edu.hse.ru>
2021-04-24 20:01:20 +0300

    update

--- transport/urls.py
+++ transport/urls.py
@@ -6,5 +6,5 @@ from . import views
 urlpatterns = [
     # path('/root/TeploEnergoHackathon/transport-web/dist/', views.file, name='file1'),
     # path('js/<str:file_name>', views.file, name='file'),
-    path('root/TeploEnergoHackathon/transport-web/dist/', views.index, name='index'),
+    path('', views.index, name='index'),
 ]
