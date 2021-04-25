227b18e0fc8e69d6bfc5cbaeed1398de9ef266cf
TolimanStaR <dadyryaev@edu.hse.ru>
2021-04-25 12:29:58 +0300

    update

--- transport/views.py
+++ transport/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, FormView
 import os
+
+from .forms import TransportCustomerForm
 
 
 # def static(request, **kwargs):
@@ -20,3 +22,22 @@ def file(request, **kwargs):
     body = open(os.path.join('transport-web/dist/', kwargs['file_name'])).read()
     print(f'\n\n{request}\n{kwargs}\n\n')
     return HttpResponse(body)
+
+
+#######################
+
+
+class TransportForm(TemplateView):
+    template_name = 'transport_form.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = TransportCustomerForm
+        return context
+
+
+class TransportFormHandle(FormView):
+
+    def form_valid(self, form):
+        print(form.cleaned_data)
+        return super().form_valid(form)
