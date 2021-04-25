be34c3cd2b5375d77bd237a431850be2f58369a6
TolimanStaR <dadyryaev@edu.hse.ru>
2021-04-25 14:57:03 +0300

    update

--- transport/models.py
+++ transport/models.py
@@ -52,3 +52,169 @@ class StaticAnalytics(models.Model):
 class AnalyticsResult(models.Model):
     image_link = models.TextField(blank=True)
     image = models.ImageField(upload_to='images/', blank=True)
+
+
+class AuthGroup(models.Model):
+    name = models.CharField(unique=True, max_length=150)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group'
+
+
+class AuthGroupPermissions(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group_permissions'
+        unique_together = (('group', 'permission'),)
+
+
+class AuthPermission(models.Model):
+    name = models.CharField(max_length=255)
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
+    codename = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_permission'
+        unique_together = (('content_type', 'codename'),)
+
+
+class AuthUser(models.Model):
+    password = models.CharField(max_length=128)
+    last_login = models.DateTimeField(blank=True, null=True)
+    is_superuser = models.BooleanField()
+    username = models.CharField(unique=True, max_length=150)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    email = models.CharField(max_length=254)
+    is_staff = models.BooleanField()
+    is_active = models.BooleanField()
+    date_joined = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user'
+
+
+class AuthUserGroups(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_groups'
+        unique_together = (('user', 'group'),)
+
+
+class AuthUserUserPermissions(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_user_permissions'
+        unique_together = (('user', 'permission'),)
+
+
+class Customer(models.Model):
+    name = models.CharField(unique=True, max_length=255)
+
+    class Meta:
+        managed = False
+        db_table = 'customer'
+
+
+class DjangoAdminLog(models.Model):
+    action_time = models.DateTimeField()
+    object_id = models.TextField(blank=True, null=True)
+    object_repr = models.CharField(max_length=200)
+    action_flag = models.SmallIntegerField()
+    change_message = models.TextField()
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'django_admin_log'
+
+
+class DjangoContentType(models.Model):
+    app_label = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'django_content_type'
+        unique_together = (('app_label', 'model'),)
+
+
+class DjangoMigrations(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    app = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    applied = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_migrations'
+
+
+class DjangoSession(models.Model):
+    session_key = models.CharField(primary_key=True, max_length=40)
+    session_data = models.TextField()
+    expire_date = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_session'
+
+
+class Event(models.Model):
+    name = models.CharField(unique=True, max_length=64)
+
+    class Meta:
+        managed = False
+        db_table = 'event'
+
+
+class License(models.Model):
+    name = models.CharField(unique=True, max_length=16)
+
+    class Meta:
+        managed = False
+        db_table = 'license'
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, models.DO_NOTHING)
+    vehicle = models.ForeignKey('Vehicle', models.DO_NOTHING)
+    license = models.ForeignKey(License, models.DO_NOTHING, blank=True, null=True)
+    predict_time = models.IntegerField()
+    date = models.CharField(max_length=16)
+    is_completed = models.BooleanField()
+
+    class Meta:
+        managed = False
+        db_table = 'order'
+
+
+class Ride(models.Model):
+    event = models.ForeignKey(Event, models.DO_NOTHING)
+    license = models.ForeignKey(License, models.DO_NOTHING, blank=True, null=True)
+    date = models.CharField(max_length=16)
+    real_time = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'ride'
+
+
+class SchemaTable(models.Model):
+    index = models.BigIntegerField(blank=True, null=True)
--- transport/views.py
+++ transport/views.py
@@ -4,10 +4,13 @@ from django.urls import reverse
 from django.views.generic import TemplateView, FormView, DetailView
 import os
 
+import psycopg2
+
 from .forms import TransportCustomerForm
 from .models import StaticAnalytics, AnalyticsResult
 
 from django.core.files.images import ImageFile
+
 
 # def static(request, **kwargs):
 #     html = open("transport-web/dist/index.html").read()
@@ -59,6 +62,18 @@ class TransportFormHandle(FormView):
 
     def form_valid(self, form):
         # print(form.cleaned_data)
+
+        conn = psycopg2.connect(dbname='main', user='postgres',
+                                password='fnvjYUbf63nv', host='193.162.143.45')
+        cursor = conn.cursor()
+
+        cursor.execute('''select l.name, o.date, "order".predict_time, o.real_time, c.name, v.name from "order"
+inner join customer c on c.id = "order".customer_id
+inner join vehicle v on v.id = "order".vehicle_id
+inner join license l on l.id = "order".license_id
+inner join "ride" o on l.id = o.license_id and "order".date = o.date;''')
+        records = cursor.fetchall()
+        print(records)
 
         m = AnalyticsResult.objects.create()
         m.image = ImageFile(open("images/dynamic.jpg", "rb"))
