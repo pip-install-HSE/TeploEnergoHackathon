0da87d224618841d68b34d6c6d0ade07b2894444
6a16ec <semaev2014@yandex.ru>
2021-04-25 12:38:29 +0300

    Wow

--- database/create_tables.py
+++ database/create_tables.py
@@ -1,4 +1,5 @@
 import os
+from tqdm import tqdm
 from dotenv import load_dotenv
 load_dotenv()
 from peewee import *
@@ -28,6 +29,8 @@ class Vehicle(BaseModel):
 class License(BaseModel):
     name = CharField(null=False, unique=True, max_length=16)
 
+class Address(BaseModel):
+    name = CharField(null=False, unique=True)
 # class Service(BaseModel):
 #     name = CharField(unique=True)
 
@@ -37,7 +40,9 @@ class Order(BaseModel):
     license = ForeignKeyField(License, null=True, backref='orders')
     predict_time = IntegerField(null=False)
     date = CharField(null=False, max_length=16)
+    address = ForeignKeyField(Address, null=False, backref='orders')
     is_completed = BooleanField(null=False)
+    is_address_match = BooleanField(null=True)
 
 class Event(BaseModel):
     name = CharField(null=False, unique=True, max_length=64)
@@ -70,27 +75,30 @@ if __name__ == '__main__':
             print(f'Can not delete {table}')
     db.create_tables(tables)
 
-    orders_data_frame = pd.read_excel(io='./orders_small.xlsx', sheet_name='Документы')
+    orders_data_frame = pd.read_excel(io='./orders.xlsx', sheet_name='Документы')
+    orders_objects_frame = pd.read_excel(io='./orders.xlsx', sheet_name='ТЧ Объекты')
 
-    for el in orders_data_frame['Заказчик']:
+    for el in tqdm(orders_data_frame['Заказчик'], total=orders_data_frame.shape[0]):
         if not Customer.select().where(Customer.name == el):
             Customer.insert(name=el).execute()
 
-    for el in orders_data_frame['Требование_кТС']:
+    for el in tqdm(orders_data_frame['Требование_кТС'], total=orders_data_frame.shape[0]):
         if not Vehicle.select().where(Vehicle.name == el):
             Vehicle.insert(name=el).execute()
 
-    for el in orders_data_frame['НазначеноТС']:
+    for el in tqdm(orders_data_frame['НазначеноТС'], total=orders_data_frame.shape[0]):
         if isinstance(el, float) and math.isnan(el):
             continue
         if not License.select().where(License.name == el):
             License.insert(name=el).execute()
 
-    for i, row in orders_data_frame.iterrows():
+    for i, row in tqdm(orders_data_frame.iterrows(), total=orders_data_frame.shape[0]):
         # if i > 5:
         #     break
-
-        is_completed = True if 'исполнен' in row['СтатусЗаявки'].lower() else False
+        is_completed = row['СтатусЗаявки']
+        if isinstance(is_completed, float) and math.isnan(is_completed):
+            continue
+        is_completed = True if 'исполнен' in is_completed.lower() else False
         start_time = row['ВремяРаботС']
         end_time = row['ВремяРаботПо']
         if not is_completed:
@@ -127,74 +135,97 @@ if __name__ == '__main__':
         # print(end_time)
         # print()
 
-        rides_data_frame = pd.read_excel(io='./rides_small.xlsx', sheet_name='Поездки по данным GPS')
+    rides_data_frame = pd.read_excel(io='./rides.xlsx', sheet_name='Поездки по данным GPS')
 
-        for el in rides_data_frame['Событие']:
-            if not Event.select().where(Event.name == el):
-                Event.insert(name=el).execute()
+    for el in tqdm(rides_data_frame['Событие'], total=rides_data_frame.shape[0]):
+        if not Event.select().where(Event.name == el):
+            Event.insert(name=el).execute()
 
-        for i, row in rides_data_frame.iterrows():
-            if row['Событие'] != 'Работа':  # everything is broken if you remove it.
-                continue
+    for i, row in tqdm(rides_data_frame.iterrows(), total=rides_data_frame.shape[0]):
+        if row['Событие'] != 'Работа':  # everything is broken if you remove it.
+            continue
 
-            # if i > 5:
-            #     break
-            license_info = row['ТС'].replace(' ', '')
-            date_info = row['Начало'][:10]
-            real_time = row['Длительноcть']
-            if isinstance(real_time, float) and math.isnan(real_time):
-                continue
-            real_time = int(real_time[0:2]) * 60 + int(real_time[3:5])
+        # if i > 5:
+        #     break
+        license_info = row['ТС'].replace(' ', '')
+        date_info = row['Начало'][:10]
+        real_time = row['Длительноcть']
+        if isinstance(real_time, float) and math.isnan(real_time):
+            continue
+        real_time = int(real_time[0:2]) * 60 + int(real_time[3:5])
 
-            try:
-                ride = Ride.select().where(
-                    Ride.date == date_info and
-                    Ride.license_id == License.select(License.id).where(License.name == license_info)
-                ).get()
-                Ride.update(real_time=ride.real_time + real_time).where(
-                    Ride.id == ride.id
-                ).execute()
-            except:
-                # print(i, license_info, real_time)
-                Ride.insert(
-                    event=Event.select().where(Event.name == row['Событие']),
-                    license=License.select().where(License.name == license_info),
-                    date=date_info,
-                    real_time=real_time
-                ).execute()
-            # dur = Ride.select(Ride.duration).where(
+        try:
+            rides = Ride.select().where(
+                Ride.license_id == License.select(License.id).where(License.name == license_info)
+            )
+            flag = False
+            cur_ride = None
+            for ride in rides:
+                if ride.date == date_info:
+                    cur_ride = ride
+                    flag = True
+                    break
+            if flag == False:
+                print(0 / 0)
+
+            # print(f'"{ride.date}"')
+            Ride.update(real_time=cur_ride.real_time + real_time).where(
+                Ride.id == cur_ride.id
+            ).execute()
+            # print("update")
+        except:
+            # print(i, license_info, real_time)
+            Ride.insert(
+                event=Event.select().where(Event.name == 'Работа'),
+                license=License.select().where(License.name == license_info),
+                date=date_info,
+                real_time=real_time
+            ).execute()
+
+
+
+
+
+
+        # Ride.insert(
+        #     event=Event.select().where(Event.name == row['Событие']),
+        #     license=License.select().where(License.name == license_info),
+        #     date=date_info,
+        #     real_time=real_time
+        # ).execute()
+        # dur = Ride.select(Ride.duration).where(
+        #     Ride.license.name == license_info and
+        #     Ride.date == date_info
+        # ).execute()
+        # if dur is None:
+        #     Ride.insert(
+        #         event=Event.select().where(Event.name == row['Событие']),
+        #         license=license_info,
+        #         date=date_info,
+        #         duration=duration
+        #     ).execute()
+        # else:
+        #     Ride.update(duration=duration+1).where(
+        #         Ride.license.name == license_info and
+        #         Ride.date == date_info
+        #     )
+            # x = Ride.update(duration=ride[1]+duration).where(
             #     Ride.license.name == license_info and
             #     Ride.date == date_info
             # ).execute()
-            # if dur is None:
-            #     Ride.insert(
-            #         event=Event.select().where(Event.name == row['Событие']),
-            #         license=license_info,
-            #         date=date_info,
-            #         duration=duration
-            #     ).execute()
-            # else:
-            #     Ride.update(duration=duration+1).where(
-            #         Ride.license.name == license_info and
-            #         Ride.date == date_info
-            #     )
-                # x = Ride.update(duration=ride[1]+duration).where(
-                #     Ride.license.name == license_info and
-                #     Ride.date == date_info
-                # ).execute()
 
 
-        # print(row['Заказчик'], row['СтатусЗаявки'])
+    # print(row['Заказчик'], row['СтатусЗаявки'])
 
-    # for line in data_frame[:5]:
-    #     print(line)
+# for line in data_frame[:5]:
+#     print(line)
 
-    # for el in data_frame['СтатусЗаявки']:
-    #     if not OrderStatus.select().where(OrderStatus.name == el):
-    #         Customer.insert(name=el).execute()
+# for el in data_frame['СтатусЗаявки']:
+#     if not OrderStatus.select().where(OrderStatus.name == el):
+#         Customer.insert(name=el).execute()
 
 
-    # for el in data_frame['ВидОперации']:
-    #     if not Service.select().where(Service.name == el):
-    #         Service.insert(name=el).execute()
+# for el in data_frame['ВидОперации']:
+#     if not Service.select().where(Service.name == el):
+#         Service.insert(name=el).execute()
 
--- database/test_alg.py
+++ database/test_alg.py
@@ -1,15 +1,62 @@
 from create_tables import *
 
 if __name__ == '__main__':
-    duration = 10
-    ride = Ride.select().where(
-        Ride.date == "12.01.2021" and
-        Ride.license_id == License.select(License.id).where(License.name=="С963АМ1512")
-    ).get()
-    print(res.duration)
-    Ride.update(duration=ride.duration+10).where(
-        Ride.id == ride.id
-    ).execute()
+    date_info = '01.01.2021'
+    license_info = 'О540УН1521'
+    real_time = 10
+
+    orders_data_frame = pd.read_excel(io='./orders.xlsx', sheet_name='Документы')
+    orders_objects_frame = pd.read_excel(io='./orders.xlsx', sheet_name='ТЧ Объекты')
+    orders_objects_numbers = orders_objects_frame['Номер']
+
+    for i, row in orders_data_frame.iterrows():
+    # for data in orders_data_frame[['Номер', 'АдресНазначения']]:
+        number = row['Номер']
+        el = row['АдресНазначения']
+        # el = data['АдресНазначения']
+        # if isinstance(el, float) and math.isnan(el):
+        #     continue
+        if number in orders_objects_numbers:
+            el = orders_objects_frame.loc[orders_objects_frame['Номер'] == number]['Объект']
+            print(number)
+
+        else:
+            pass
+        # print(number, el)
+            # el = orders_objects_frame['Объект']
+        # if not License.select().where(License.name == el):
+        #     License.insert(name=el).execute()
+
+    # Event.insert(name='Работа').execute()
+
+    # try:
+    #     rides = Ride.select().where(
+    #         Ride.license_id == License.select(License.id).where(License.name == license_info)
+    #     )
+    #     flag = False
+    #     cur_ride = None
+    #     for ride in rides:
+    #         if ride.date == date_info:
+    #             cur_ride = ride
+    #             flag = True
+    #             break
+    #     if flag == False:
+    #         print(0 / 0)
+    #
+    #     # print(f'"{ride.date}"')
+    #     Ride.update(real_time=ride.real_time + real_time).where(
+    #         Ride.id == cur_ride.id
+    #     ).execute()
+    #     # print("update")
+    # except:
+    #     # print(i, license_info, real_time)
+    #     Ride.insert(
+    #         event=Event.select().where(Event.name == 'Работа'),
+    #         license=License.select().where(License.name == license_info),
+    #         date=date_info,
+    #         real_time=real_time
+    #     ).execute()
+
     # print(res.exists())
     # try:
     # ride = Ride.select().join(License).where(
